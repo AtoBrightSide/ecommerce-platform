cd027b4ecf28f28593eb78e90fb84e22cc5863c6
Abel Mesele <meseleabel99@gmail.com>
2022-01-30 19:17:50 +0300

    feat: add sign in and sign up page

---
+++ src/routes/SignInPage.tsx
@@ -0,0 +1,26 @@
+const SignInPage = () => {
+    return (
+        <>
+            <div className="flex my-5">
+                <div className="w-1/2">
+                    <img src="/src/assets/dl.beatsnoop 1 (1).png" alt="" />
+                </div>
+                <div className="flex flex-col items-center w-1/2 mt-28">
+                    <div className="flex flex-col space-y-5">
+                        <div className="text-4xl font-semibold">Login to Everyone Shops</div>
+                        <div className="">Enter your details below</div>
+                        <input type="email" placeholder="Email or phone number" className="border-0 rounded-lg ring-1 ring-inset ring-gray-300 focus:ring-indigo-600 p-2 focus:ring-2 ease-in duration-75" />
+                        <input type="password" placeholder="Password" className="border-0 rounded-lg ring-1 ring-inset ring-gray-300 focus:ring-indigo-600 p-2 focus:ring-2 ease-in duration-75" />
+
+                        <div className="flex justify-between items-center">
+                            <button className="w-1/3 h-14 bg-[#DB4444] text-white rounded-md hover:bg-[#db4444cb]">Login</button>
+                            <div className="text-[#DB4444] cursor-pointer hover:text-[#db4444bd]">Forget Password?</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default SignInPage
---
+++ src/routes/SignUpPage.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+const SignUpPage = () => {
+    return (
+        <>
+            <div className="flex my-5">
+                <div className="w-1/2">
+                    <img src="/src/assets/dl.beatsnoop 1 (1).png" alt="" />
+                </div>
+                <div className="flex flex-col items-center w-1/2 mt-28">
+                    <div className="flex flex-col space-y-5">
+                        <div className="text-4xl font-semibold">Create An Account</div>
+                        <div className="">Enter your details below</div>
+                        <input type="text" placeholder="Name" className="border-0 rounded-lg ring-1 ring-inset ring-gray-300 focus:ring-indigo-600 p-2 focus:ring-2 ease-in duration-75" />
+                        <input type="email" placeholder="Email or phone number" className="border-0 rounded-lg ring-1 ring-inset ring-gray-300 focus:ring-indigo-600 p-2 focus:ring-2 ease-in duration-75" />
+                        <input type="password" placeholder="Password" className="border-0 rounded-lg ring-1 ring-inset ring-gray-300 focus:ring-indigo-600 p-2 focus:ring-2 ease-in duration-75" />
+
+                        <div className="space-y-2">
+                            <button className="w-full h-14 bg-[#DB4444] text-white rounded-md hover:bg-[#db4444cb]">Create Account</button>
+                            <button className="w-full h-14 border border-black rounded-md hover:bg-[#d0d0d0] hover:text-white">Sign up with Google</button>
+                            <div className="">Already have an account? <Link to="/signin" className="font-semibold hover:underline hover:font-bold ease-in duration-75">Log In</Link></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default SignUpPage
