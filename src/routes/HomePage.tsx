7aed3b702a912f7f60859ca2dff209dc77677ac9
Abel Mesele <meseleabel99@gmail.com>
2022-01-31 03:57:18 +0300

    fix: update countdown components

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
             <SideNav />
 
             <div className="w-full mt-[140px] md:px-56">
-                <TodaySection countDownDate={new Date("Jul 10, 2024 15:37:25").getTime()} />
+                <TodaySection countDownDate={new Date("Jul 10, 2025 15:37:25").getTime()} />
 
                 <CategorySection />
 
